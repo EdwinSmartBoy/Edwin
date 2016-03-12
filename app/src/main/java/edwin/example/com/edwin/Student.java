36bddb749d16027534f172a9e6ca5cf71d7984ca
EdwinSmartBoy <602546370@qq.com>
2016-03-12 18:36:39 +0800

    创建学生类

---
+++ app/src/main/java/edwin/example/com/edwin/Student.java
@@ -0,0 +1,56 @@
+package edwin.example.com.edwin;
+
+/**
+ * Created by 长旋 on 2016/3/12.
+ * <p/>
+ * 学生类的封装
+ */
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private int grade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public Student(int id, String name, int age, int grade) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public Student() {
+    }
+}
